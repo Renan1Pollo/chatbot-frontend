6fa9fdf59725bf8b476c784a46e39e8c73016667
Renan Pollo Benelli <renansk18@outlook.com>
2024-04-26 15:50:28 -0300

    Feat: Add Icons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
+import { ChatComponent } from './pages/chat/chat.component';
 import { HomeComponent } from './pages/home/home.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
+  { path: "", component: ChatComponent },
 ];
